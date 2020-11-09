eb71c99dab1b1d66719baf08c68c457e72cbe066
Alexey Egin <DominuS-RU@users.noreply.github.com>
2020-11-09 22:05:10 +0300

    Fix api version constant (#145)
    * Fix api version constant
    
    * Update build.gradle

--- app/src/main/java/ru/poprobuy/poprobuy/util/Constants.kt
+++ app/src/main/java/ru/poprobuy/poprobuy/util/Constants.kt
@@ -11,7 +11,7 @@ object Constants {
 
   // Network
   const val POPROBUY_API_ENDPOINT = "${BuildConfig.BASE_URL}/api/"
-  const val POPROBUY_API_VERSION = BuildConfig.VERSION_CODE
+  const val POPROBUY_API_VERSION = BuildConfig.API_VERSION
 
   // Urls
   const val HELP_SCAN_RECEIPT_URL = "https://poprobuy.ru" // TODO: 14.07.2020 Change url
