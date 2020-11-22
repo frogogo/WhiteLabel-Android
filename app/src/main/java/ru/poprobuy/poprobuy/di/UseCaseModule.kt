4d0b76f7023a03f18bb21a99d2b5c3ad02d6ca66
Alexey Egin <DominuS-RU@users.noreply.github.com>
2020-11-22 19:59:04 +0300

    Make use cases singleton (#185)

--- app/src/main/java/ru/poprobuy/poprobuy/di/UseCaseModule.kt
+++ app/src/main/java/ru/poprobuy/poprobuy/di/UseCaseModule.kt
@@ -16,26 +16,26 @@ import ru.poprobuy.poprobuy.usecase.vending_machine.AssignVendingMachineUseCase
 
 val useCaseModule = module {
   // Auth
-  factory { GetUserAuthStateUseCase(get()) }
-  factory { RequestConfirmationCodeUseCase(get()) }
-  factory { AuthenticationUseCase(get(), get()) }
-  factory { RefreshTokenUseCase(get(), get()) }
-  factory { RefreshTokenWorkerUseCase(get(), get(), get()) }
+  single { GetUserAuthStateUseCase(get()) }
+  single { RequestConfirmationCodeUseCase(get()) }
+  single { AuthenticationUseCase(get(), get()) }
+  single { RefreshTokenUseCase(get(), get()) }
+  single { RefreshTokenWorkerUseCase(get(), get(), get()) }
 
   // Home
-  factory { GetHomeUseCase(get()) }
+  single { GetHomeUseCase(get()) }
 
   // User
-  factory { UpdateUserDetailsUseCase(get()) }
-  factory { GetUserInfoUseCase(get()) }
+  single { UpdateUserDetailsUseCase(get()) }
+  single { GetUserInfoUseCase(get()) }
 
   // Receipt
-  factory { GetReceiptsUseCase(get()) }
-  factory { CreateReceiptUseCase(get()) }
+  single { GetReceiptsUseCase(get()) }
+  single { CreateReceiptUseCase(get()) }
 
   // Vending Machine
-  factory { AssignVendingMachineUseCase(get()) }
+  single { AssignVendingMachineUseCase(get()) }
 
   // System
-  factory { ClearUserDataUseCase(get()) }
+  single { ClearUserDataUseCase(get()) }
 }
