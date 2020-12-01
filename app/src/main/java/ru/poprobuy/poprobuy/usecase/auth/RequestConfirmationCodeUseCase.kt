e2f5e75a559f8c83c914ca102dbd619536987210
Alexey Egin <DominuS-RU@users.noreply.github.com>
2020-12-01 21:25:50 +0300

    Refactor use cases (#201)

--- app/src/main/java/ru/poprobuy/poprobuy/usecase/auth/RequestConfirmationCodeUseCase.kt
+++ app/src/main/java/ru/poprobuy/poprobuy/usecase/auth/RequestConfirmationCodeUseCase.kt
@@ -2,9 +2,12 @@ package ru.poprobuy.poprobuy.usecase.auth
 
 import com.github.ajalt.timberkt.e
 import com.github.ajalt.timberkt.i
+import ru.poprobuy.poprobuy.data.model.api.ErrorResponse
+import ru.poprobuy.poprobuy.data.model.api.auth.ConfirmationCodeRequestResponse
 import ru.poprobuy.poprobuy.data.repository.AuthRepository
 import ru.poprobuy.poprobuy.util.Result
 import ru.poprobuy.poprobuy.util.network.HttpStatus
+import ru.poprobuy.poprobuy.util.network.NetworkError
 import ru.poprobuy.poprobuy.util.network.onHttpErrorWithCode
 
 class RequestConfirmationCodeUseCase(
@@ -14,20 +17,23 @@ class RequestConfirmationCodeUseCase(
   suspend operator fun invoke(
     phoneNumber: String,
   ): RequestConfirmationResult = when (val result = authRepository.requestConfirmationCode(phoneNumber)) {
-    is Result.Success -> {
-      i { "Code requested successfully" }
-      RequestConfirmationResult.Success(result.value.passwordRefreshRate)
-    }
-    is Result.Failure -> {
-      val error = result.error
-      error.onHttpErrorWithCode(HttpStatus.TOO_MANY_REQUESTS_429) {
-        e { "TooManyRequests while requesting code" }
-        return RequestConfirmationResult.TooManyRequests
-      }
+    is Result.Success -> handleSuccess(result.value)
+    is Result.Failure -> handleFailure(result.error)
+  }
 
-      e { "Generic error while requesting code" }
-      RequestConfirmationResult.Error
+  private fun handleSuccess(value: ConfirmationCodeRequestResponse): RequestConfirmationResult {
+    i { "Code requested successfully" }
+    return RequestConfirmationResult.Success(value.passwordRefreshRate)
+  }
+
+  private fun handleFailure(error: NetworkError<ErrorResponse>): RequestConfirmationResult {
+    error.onHttpErrorWithCode(HttpStatus.TOO_MANY_REQUESTS_429) {
+      e { "TooManyRequests while requesting code" }
+      return RequestConfirmationResult.TooManyRequests
     }
+
+    e { "Generic error while requesting code" }
+    return RequestConfirmationResult.Error
   }
 
 }
