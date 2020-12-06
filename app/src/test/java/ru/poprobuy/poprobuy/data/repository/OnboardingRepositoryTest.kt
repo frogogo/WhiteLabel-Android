8b399894bacf358c4271d1dc424efd62a54e1117
Alexey Egin <DominuS-RU@users.noreply.github.com>
2020-12-06 14:06:47 +0300

    Add missing onboarding repository test (#225)

--- app/src/test/java/ru/poprobuy/poprobuy/data/repository/OnboardingRepositoryTest.kt
+++ app/src/test/java/ru/poprobuy/poprobuy/data/repository/OnboardingRepositoryTest.kt
@@ -1,25 +1,46 @@
 package ru.poprobuy.poprobuy.data.repository
 
+import io.mockk.confirmVerified
 import io.mockk.mockk
 import io.mockk.verify
+import org.amshove.kluent.shouldHaveSize
 import org.junit.Before
 import org.junit.Test
 import ru.poprobuy.poprobuy.data.preferences.UserPreferences
 
 class OnboardingRepositoryTest {
 
-  private lateinit var onboardingRepository: OnboardingRepository
+  private lateinit var repository: OnboardingRepository
+
   private val userPreferences: UserPreferences = mockk(relaxed = true)
 
   @Before
   fun startUp() {
-    onboardingRepository = OnboardingRepository(userPreferences)
+    repository = OnboardingRepository(
+      userPreferences = userPreferences
+    )
   }
 
   @Test
-  fun `onboarding state is stored to shared preferences`() {
-    onboardingRepository.setOnboardingCompleted()
-    verify { userPreferences.onboardingCompleted = true }
+  fun `repository should return onboarding pages`() {
+    val pages = repository.getPages()
+
+    pages shouldHaveSize 3
+    confirmVerified()
+  }
+
+  @Test
+  fun `repository should save onboarding state`() {
+    repository.setOnboardingCompleted()
+
+    verify {
+      userPreferences.onboardingCompleted = true
+    }
+    confirmVerified()
+  }
+
+  private fun confirmVerified() {
+    confirmVerified(userPreferences)
   }
 
 }
