b0e645f03ff1ce2135ec8dca8bd0ad55c13fd7dd
Alexey Egin <DominuS-RU@users.noreply.github.com>
2020-12-06 13:31:25 +0300

    Add DateExt tests (#221)

---
+++ app/src/test/java/ru/poprobuy/poprobuy/extension/DateExtKtTest.kt
@@ -0,0 +1,25 @@
+package ru.poprobuy.poprobuy.extension
+
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+import ru.poprobuy.poprobuy.toDate
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+internal class DateExtKtTest {
+
+  @Test
+  fun toDateTime() {
+    val date = LocalDateTime.of(2020, 10, 5, 10, 20).toDate()
+
+    date.toDateTime() shouldBeEqualTo "05.10.2020 - 10:20"
+  }
+
+  @Test
+  fun toSimpleDate() {
+    val date = LocalDate.of(2020, 10, 5).toDate()
+
+    date.toSimpleDate() shouldBeEqualTo "05.10.2020"
+  }
+
+}
