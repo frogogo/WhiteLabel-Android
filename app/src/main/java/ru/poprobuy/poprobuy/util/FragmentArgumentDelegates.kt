3f294f87a75303ef451f598dc7fc1b7c40688247
Alexey Egin <10329086+DominuS-RU@users.noreply.github.com>
2021-01-30 16:41:43 +0300

    Refactor fragment argument delegate (#264)

--- app/src/main/java/ru/poprobuy/poprobuy/util/FragmentArgumentDelegates.kt
+++ app/src/main/java/ru/poprobuy/poprobuy/util/FragmentArgumentDelegates.kt
@@ -3,7 +3,5 @@ package ru.poprobuy.poprobuy.util
 import androidx.fragment.app.Fragment
 
 inline fun <reified T> Fragment.argument(name: String): Lazy<T> = lazy {
-  val arguments = arguments
-  requireNotNull(arguments) { "arguments must be supplied" }
-  arguments.get(name) as T
+  requireArguments().get(name) as T
 }
