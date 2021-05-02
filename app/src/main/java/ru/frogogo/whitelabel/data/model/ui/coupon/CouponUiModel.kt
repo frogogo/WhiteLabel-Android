e06318268fa3b713254fe7679d3f6d84af972ce1
Alexey Egin <egin9@mail.ru>
2021-05-02 19:45:12 +0300

    Implement opening coupon info

--- app/src/main/java/ru/frogogo/whitelabel/data/model/ui/coupon/CouponUiModel.kt
+++ app/src/main/java/ru/frogogo/whitelabel/data/model/ui/coupon/CouponUiModel.kt
@@ -1,14 +1,17 @@
 package ru.frogogo.whitelabel.data.model.ui.coupon
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import ru.frogogo.whitelabel.core.recycler.RecyclerViewItem
 
+@Parcelize
 data class CouponUiModel(
   val id: Int,
   val name: String,
   val description: String,
   val image: CouponImageUiModel,
   val qrString: String,
-) : RecyclerViewItem {
+) : RecyclerViewItem, Parcelable {
 
   override fun getId(): Any = "ITEM_COUPON$id"
 }
--- app/src/main/java/ru/frogogo/whitelabel/di/scope/CouponInfoScope.kt
+++ app/src/main/java/ru/frogogo/whitelabel/di/scope/CouponInfoScope.kt
@@ -4,11 +4,11 @@ package ru.frogogo.whitelabel.di.scope
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import ru.frogogo.whitelabel.ui.coupon_info.CouponInfoFragment
 import ru.frogogo.whitelabel.ui.coupon_info.CouponInfoViewModel
-import ru.frogogo.whitelabel.ui.home.HomeFragment
 
 fun Module.couponInfo() {
-  scope<HomeFragment> {
+  scope<CouponInfoFragment> {
     // Core
     viewModel { CouponInfoViewModel(get(), get()) }
 
